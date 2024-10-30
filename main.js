01c17251ee7b5d18d1bf72daa75be3524a8869f5
Alexander Herta <alexanderherta2002@proton.me>
2024-10-30 17:35:34 +1100

    Display error messages when given invalid input

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const firstNameInput = document.querySelector("#first-name-input");
 const lastNameInput = document.querySelector("#last-name-input");
 const emailInput = document.querySelector("#email-input");
 const passwordInput = document.querySelector("#password-input");
+const firstNameError = document.querySelector("#first-name-error");
+const lastNameError = document.querySelector("#last-name-error");
+const emailError = document.querySelector("#email-error");
+const passwordError = document.querySelector("#password-error");
+
 let isFirstNameValid = false;
 let isLastNameValid = false;
 let isEmailValid = false;
@@ -11,34 +16,43 @@ function checkInput() {
   const firstName = firstNameInput.value;
   const firstNameRegex = /[^A-Za-z0-9]/;
   if(!/\s/.test(firstName) && !firstNameRegex.test(firstName) && !/\d/.test(firstName) && firstName.length < 27) {
-    //input good
+    isFirstNameValid = true;
+    firstNameError.innerText = "";
   } else {
-    //first name error
+    firstNameError.innerText = "First name cannot be empty, contain spaces, or contain numbers.";
   }
 
   const lastName = lastNameInput.value;
   const lastNameRegex = /[^A-Za-z0-9]/;
   if(!/\s/.test(lastName) && !lastNameRegex.test(lastName) && !/\d/.test(lastName) && lastName.length < 27) {
-    //input good
+    isLastNameValid = true;
+    lastNameError.innerText = "";
   } else {
-    // last name error
+    lastNameError.innerText = "Last name cannot be empty, contain spaces, or contain numbers.";
   }
 
   const email = emailInput.value;
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if(emailRegex.test(email)) {
-    //input good
-    alert("good email");
+    isEmailValid = true;
+    emailError.innerText = "";
   } else {
-    // email error
-    alert("not email");
+    emailError.innerText = "Looks like this is not an email.";
   }
 
   const password = passwordInput.value;
-  if(password.length < 41) {
-    //input good
+  if(!/\s/.test(password) && password.length < 41) {
+    isPasswordValid = true;
+    passwordError.innerText = "";
   } else {
-    //pass error
+    passwordError.innerText = "Password cannot be too long or contain spaces.";
   }
 
+  if(isFirstNameValid && isLastNameValid && isEmailValid && isPasswordValid) {
+    displayThankYou();
+  }
+}
+
+function displayThankYou() {
+  alert("vaild input!");
 }

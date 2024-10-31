d32cfb93d8b9f3493fa84efb6bbc6bb6cf4cd054
Alexander Herta <alexanderherta2002@proton.me>
2024-10-31 12:00:05 +1100

    Add red border and error icon to inputs

--- main.js
+++ main.js
@@ -18,8 +18,12 @@ function checkInput() {
   if(!/\s/.test(firstName) && !firstNameRegex.test(firstName) && !/\d/.test(firstName) && firstName.length < 27) {
     isFirstNameValid = true;
     firstNameError.innerText = "";
+    firstNameInput.style.borderColor = "rgba(0, 0, 0, 0.25)";
+    document.documentElement.style.setProperty("--error-icon-first", "none");
   } else {
     firstNameError.innerText = "First name cannot be empty, contain spaces, or contain numbers.";
+    firstNameInput.style.borderColor = "red";
+    document.documentElement.style.setProperty("--error-icon-first", "block");
   }
 
   const lastName = lastNameInput.value;
@@ -27,8 +31,12 @@ function checkInput() {
   if(!/\s/.test(lastName) && !lastNameRegex.test(lastName) && !/\d/.test(lastName) && lastName.length < 27) {
     isLastNameValid = true;
     lastNameError.innerText = "";
+    lastNameInput.style.borderColor = "rgba(0, 0, 0, 0.25)";
+    document.documentElement.style.setProperty("--error-icon-last", "none");
   } else {
     lastNameError.innerText = "Last name cannot be empty, contain spaces, or contain numbers.";
+    lastNameInput.style.borderColor = "red";
+    document.documentElement.style.setProperty("--error-icon-last", "block");
   }
 
   const email = emailInput.value;
@@ -36,16 +44,24 @@ function checkInput() {
   if(emailRegex.test(email)) {
     isEmailValid = true;
     emailError.innerText = "";
+    emailInput.style.borderColor = "rgba(0, 0, 0, 0.25)";
+    document.documentElement.style.setProperty("--error-icon-email", "none");
   } else {
     emailError.innerText = "Looks like this is not an email.";
+    emailInput.style.borderColor = "red";
+    document.documentElement.style.setProperty("--error-icon-email", "block");
   }
 
   const password = passwordInput.value;
   if(!/\s/.test(password) && password.length < 41) {
     isPasswordValid = true;
     passwordError.innerText = "";
+    passwordInput.style.borderColor = "rgba(0, 0, 0, 0.25)";
+    document.documentElement.style.setProperty("--error-icon-password", "none");
   } else {
     passwordError.innerText = "Password cannot be too long or contain spaces.";
+    passwordInput.style.borderColor = "red";
+    document.documentElement.style.setProperty("--error-icon-password", "block");
   }
 
   if(isFirstNameValid && isLastNameValid && isEmailValid && isPasswordValid) {
